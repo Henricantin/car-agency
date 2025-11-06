7efa11aa1416f6c4c8f13fdde6cedbbb22e387b0
Henricantin <henrique.barrosleo@hotmail.com>
2025-11-06 15:50:40 -0300

    fix db

--- my_db.sql
+++ my_db.sql
@@ -2,7 +2,6 @@ DROP DATABASE IF EXISTS car_agency;
 CREATE DATABASE car_agency;
 
 USE car_agency;
-
 CREATE TABLE customers(
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -12,7 +11,6 @@ CREATE TABLE customers(
     );
 
 USE car_agency;
-
 CREATE TABLE vehicles(
     id INT AUTO_INCREMENT PRIMARY KEY,
     brand VARCHAR(50) NOT NULL,
@@ -22,7 +20,6 @@ CREATE TABLE vehicles(
     );
 
 USE car_agency;
-
 CREATE TABLE sales(
     id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT NOT NULL,
@@ -34,11 +31,11 @@ CREATE TABLE sales(
     );
 
 USE car_agency;
-
 INSERT INTO customers(name, email, phone, address) VALUES('Henrique Cantin', 'henrique.cantin@gmail.com', '11940401212', 'Rua Brasil, SN'),
 ('Lucca Bento', 'luccabento@gmail.com', '11987654321', 'Avenida Paulista, 1000'),
 ('Natalia Silva', 'nati.s@outlook.com', '11912345678', 'Rua das Flores, 50'),
-('Maria Julia', 'maju@hotmail.com', '11923456789', 'Alameda Santos, 200');
+('Maria Julia', 'maju@hotmail.com', '11923456789', 'Alameda Santos, 200'),
+('Leonardo Dantas', 'dantas@outlook.com', '11945698787', 'Av Campo Limpo, 50');
 
 USE car_agency;
 INSERT INTO sales (customer_id, vehicle_id, price) VALUES
@@ -47,8 +44,8 @@ INSERT INTO sales (customer_id, vehicle_id, price) VALUES
 (3, 3, 75000.00);
 
 USE car_agency;
-INSERT INTO vehicles (brand, model, year, price, cor) VALUES
-('Honda', 'Civic', 2005, 50000.00, 'Prata'),
-('Toyota', 'Corolla', 2010, 42000.00, 'Preto'),
-('Ford', 'Focus', 2014, 75000.00, 'Branco'),
-('BMW', '320i', 2018, 120000.00, 'Azul');
+INSERT INTO vehicles (brand, model, year, color, price) VALUES
+('Honda', 'Civic', 2005, 'Prata', 50000.00),
+('Toyota', 'Corolla', 2010, 'Preto', 42000.00),
+('Ford', 'Focus', 2014, 'Branco', 75000.00),
+('BMW', '320i', 2018, 'Azul', 120000.00);
